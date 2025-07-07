f55e0aca24bc77f97e98591ac98045f9ef64ac69
ubuntu <kce0508@gmail.com>
2025-07-07 18:44:00 +0900

    커밋

--- Socket/chapter6/http_server.c
+++ Socket/chapter6/http_server.c
@@ -7,81 +7,88 @@
 
 #define BUF_SIZE 1024
 
-void error_handling(const char *msg);
-
-int main()
+int main(int argc, char** argv)
 {
-    int serv_sock, clnt_sock;
-    char request[BUF_SIZE];
-    struct sockaddr_in serv_adr, clnt_adr;
-    socklen_t clnt_adr_sz;
+        int serv_sock, clnt_sock;
+        struct sockaddr_in serv_addr;
+        char buf[BUF_SIZE];
 
-    char webpage[] = "HTTP/1.1 200 OK\r\n"
-        "Server: Linux Web Server\r\n"
-        "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-        "<!DOCTYPE html>\r\n"
-        "<html><head><title> My Web Page </title>\r\n"
-        "<style>body {background-color: #FFFF00 }</style></head>\r\n"
-        "<body><center><h1>Hello world!!</h1><br>\r\n"
-        "<img src=\"Sylvanian.jpg\"></center></body></html>\r\n";
+        char webpage[] = "HTTP/1.1 200 OK\r\n"
+                "Server:Linux Web Server\r\n"
+                "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+                "<!DOCTYPE html>\r\n"
+                "<html><head><title> My Web Page </title>\r\n"
+                "<style>body {background-color: #FFC0CB}</style></head>\r\n"
+                "<body><center><h1>Hello World!</h1><br>\r\n"
+                "<img src=\"Sylva.jpg\" width=\"500\"></center></body></html>\r\n";
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1)
-        error_handling("socket() error");
+        if (argc != 2) {
+                fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+                exit(1);
+        }
 
-    memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family = AF_INET;
-    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_adr.sin_port = htons(8080); // 브라우저 접속 포트
+        serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+        if (serv_sock == -1) {
+                perror("socket() error");
+                exit(1);
+        }
 
-    if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
-        error_handling("bind() error");
+        memset(&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (listen(serv_sock, 5) == -1)
-        error_handling("listen() error");
+        if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+                perror("bind() error");
+                close(serv_sock);
+                exit(1);
+        }
 
-    printf("웹서버 실행 중... 브라우저에서 http://<IP>:8080 으로 접속해보세요\n");
+        if (listen(serv_sock, 5) == -1) {
+                perror("listen() error");
+                close(serv_sock);
+                exit(1);
+        }
 
-    clnt_adr_sz = sizeof(clnt_adr);
-    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+        while (1) {
+                clnt_sock = accept(serv_sock, NULL, NULL);
+                if (clnt_sock == -1) {
+                        perror("accept() error");
+                        continue;
+                }
 
-    read(clnt_sock, request, BUF_SIZE - 1);
-    printf("요청 메시지 수신:\n%s\n", request);
+                int str_len = read(clnt_sock, buf, BUF_SIZE - 1);
+                if (str_len <= 0) {
+                        perror("read() error");
+                        close(clnt_sock);
+                        continue;
+                }
+                buf[str_len] = 0;
 
-	if (strstr(request, "GET /Sylvanian.jpg") != NULL) {
-		FILE *img = fopen("Sylvanian.jpg", "rb");
-		if(img == NULL) {
-			error_handling("Image file open failed");
-		}
+                if (strstr(buf, "GET /Sylva.jpg") != NULL) {
+                        FILE* fp = fopen("Sylva.jpg", "rb");
+                        if (fp == NULL) {
+                                perror("Image file open failed");
+                                close(clnt_sock);
+                                continue;
+                        }
 
-		char img_header[] =
-			"HTTP/1.1 200 OK\r\n"
-			"Content-Type: image/jpeg\r\n\r\n";
-		write(clnt_sock, img_header, strlen(img_header));
+                        char img_header[] = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
+                        write(clnt_sock, img_header, strlen(img_header));
 
-		char img_buf[BUF_SIZE];
-		int read_cnt;
-		while ((read_cnt = fread(img_buf, 1, BUF_SIZE, img)) > 0) {
-			write(clnt_sock, img_buf, read_cnt);
-		}
-		fclose(img);
-	}
-    // GET 또는 POST 처리
-    else if (strncmp(request, "GET", 3) == 0 || strncmp(request, "POST", 4) == 0) {
-        write(clnt_sock, webpage, strlen(webpage));
-    } else {
-        char notfound[] = "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n";
-        write(clnt_sock, notfound, strlen(notfound));
-    }
+                        char img_buf[BUF_SIZE];
+                        int read_bytes;
+                        while ((read_bytes = fread(img_buf, 1, BUF_SIZE, fp)) > 0)
+                                write(clnt_sock, img_buf, read_bytes);
+                        fclose(fp);
+                }
+                else {
+                        write(clnt_sock, webpage, strlen(webpage));
+                }
 
-    close(clnt_sock);
-    close(serv_sock);
-    return 0;
-}
+                close(clnt_sock);
+        }
 
-void error_handling(const char *msg)
-{
-    fputs(msg, stderr);
-    fputc('\n', stderr);
-    exit(1);
+        close(serv_sock);
+        return 0;
 }
